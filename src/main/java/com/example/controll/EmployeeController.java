f0b34fd1ba43d6c68f34dfc3698217d3adccfc76
emrulla muhter <emrullauigur@gmai.com>
2023-08-24 20:39:03 +0300

    backand part

--- src/main/java/com/example/controll/EmployeeController.java
+++ src/main/java/com/example/controll/EmployeeController.java
@@ -3,8 +3,10 @@ package com.example.controll;
 import com.example.bootstrap.DataGenerater;
 import com.example.model.Employee;
 import com.example.service.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,9 +28,16 @@ model.addAttribute("employee",new Employee());
       return "employee/employee-create";
 }
 @PostMapping("/insert")
-public String insertEmployee(@ModelAttribute ("employee") Employee employee){
+public String insertEmployee(@ModelAttribute ("employee") @Valid Employee employee , BindingResult bindingResult ,Model model){
+if (bindingResult.hasErrors()){
+    model.addAttribute("stateList", DataGenerater.getAllStates());
+
+    return "employee/employee-create";
+}
+
+
   employeeService.saveEmployee(employee);
-  return " redirect:/employee/list";    //with redirect we are using endpoint
+    return "redirect:/employee/list";    //with redirect we are using endpoint
 
 }
 @GetMapping("/list")
--- src/main/java/com/example/model/Employee.java
+++ src/main/java/com/example/model/Employee.java
@@ -1,5 +1,6 @@
 package com.example.model;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,25 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class Employee {
+   // @NotNull -->fild shouldnt be null
+    //@NotBlank -->fild shouldnt be ""
+    //@NotEmpty -->fild should not be "       "
+
+    // @NotNull -->@Notnull
+    //@NotBlank -->@Notnull+@Notempty
+    //@NotEmpty -->@Notnull+@Notempty+NotBlank
+
+   @NotBlank
+   @Size(max = 12,min = 2)
     private String firstName;
-    private String lastName;
+   private String lastName;
     @DateTimeFormat(pattern = "yyyy-mm-dd")
     private LocalDate birthDay;
+    @NotBlank
+    @Email
     private String email;
+@NotBlank
+@Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String passWord;
     private String address;
     private String  address2;
